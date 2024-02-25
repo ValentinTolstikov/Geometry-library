5158f893a487d07d986badff9e54608b5abe6e90
ValentinTolstikov <tolstoivalek@yandex.ru>
2024-02-25 21:52:31 +0300

    В классе Triangle добавлены функции для вычисления площади, периметра треугольника, а также проверки существует ли он.

---
+++ GeometryLib/GeometryOperations.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryLib
+{
+    public class Circle
+    {
+        /// <summary>
+        /// Функция, позволяющая вычислить площадь круга.
+        /// </summary>
+        /// <param name="Radius">Радиус круга</param>
+        /// <returns>Возвращает площадь, если введенный радиус </returns>
+        public static double GetArea(double Radius)
+        {
+            if (Radius <= 0)
+            {
+                return double.NaN;
+            }
+            
+            return Math.PI * (Math.Pow(Radius,2));
+        }
+    }
+
+    public class Triangle
+    {
+        /// <summary>
+        /// Функция, позволяющая вычислить площадь треугольника по сторонам.
+        /// </summary>
+        /// <param name="SideA"></param>
+        /// <param name="SideB"></param>
+        /// <param name="SideC"></param>
+        /// <returns></returns>
+        public static double GetArea(double SideA, double SideB, double SideC)
+        {
+            if (IsExist(SideA, SideB, SideC))
+            {
+                
+            }
+
+            double Per = GetPerimeter(SideA, SideB, SideC);
+            if (double.IsNaN(Per))
+            {
+                return double.NaN;
+            }
+            double HalfPer = Per / 2;
+
+            return Math.Sqrt(HalfPer*((HalfPer-SideA)*(HalfPer-SideB)*(HalfPer-SideC)));
+        }
+
+        public static double GetPerimeter(double SideA, double SideB, double SideC)
+        {
+            if (SideA <= 0 || SideB <= 0 || SideC <= 0)
+            {
+                return double.NaN;
+            }
+
+            return (SideA+SideB+SideC)/2;
+        }
+
+        public static bool IsExist(double SideA, double SideB, double SideC)
+        {
+            return (SideA + SideB > SideC) && (SideA + SideC > SideB) && (SideB + SideC > SideA);
+        }
+    }
+}
